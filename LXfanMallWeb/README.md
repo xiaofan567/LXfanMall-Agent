c725db8d54d67490dafa63f24f13c9f9810925b9
xiaofan567 <2829512515@qq.com>
2026-06-19 16:42:33 +0800

    chore: 补充各子项目配置与锁定文件
    - .gitignore、.prettierrc、eslint 配置
    - package-lock.json 依赖锁定
    - LICENSE、README 等项目元信息

---
+++ LXfanMallWeb/README.md
@@ -0,0 +1,11 @@
+
+  # LXfanMall电商平台官网
+
+  This is a code bundle for LXfanMall电商平台官网. The original project is available at https://www.figma.com/design/xk1mgyZd23jWjGK6Nt9xe8/LXfanMall%E7%94%B5%E5%95%86%E5%B9%B3%E5%8F%B0%E5%AE%98%E7%BD%91.
+
+  ## Running the code
+
+  Run `npm i` to install the dependencies.
+
+  Run `npm run dev` to start the development server.
+  
---
+++ LXfanMallWeb/eslint.config.js
@@ -0,0 +1,39 @@
+import js from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import react from 'eslint-plugin-react'
+import reactHooks from 'eslint-plugin-react-hooks'
+import jsxA11y from 'eslint-plugin-jsx-a11y'
+import globals from 'globals'
+
+export default tseslint.config(
+  { ignores: ['dist', 'node_modules', '*.config.js'] },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      'jsx-a11y': jsxA11y,
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    rules: {
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
+      ...reactHooks.configs.recommended.rules,
+      ...jsxA11y.configs.recommended.rules,
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/no-explicit-any': 'warn',
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+    },
+  },
+)
